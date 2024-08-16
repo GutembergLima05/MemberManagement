409973b78b69014e60c9c24d7a44c66b9c7ef946
Gutemberg Lima <j.berglima05@gmail.com>
2024-08-16 20:49:09 -0300

    Domain policy implemented

---
+++ Project.Domain/Abstractions/IMemberRepository.cs
@@ -0,0 +1,17 @@
+﻿using Project.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Abstractions;
+
+public interface IMemberRepository
+{
+    Task<IEnumerable<Member>> GetAllMembers();
+    Task<Member> GetMemberById(int id);
+    Task<Member> AddMember(Member member);
+    Task<Member> UpdateMember(Member member);
+    Task DeleteMemberById(int memberId);
+}
